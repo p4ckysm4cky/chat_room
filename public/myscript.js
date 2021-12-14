134ce69075fbb678326e0b52f805f4ab845d19b8
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2021-12-14 23:14:30 +1300

    added functions to make the client side functional

--- public/myscript.js
+++ public/myscript.js
@@ -3,8 +3,9 @@ const messagesUl = document.getElementById("messages");
 const messageBoxInput = document.getElementById("messageBox");
 
 function main() {
+    initInput();
     socket.on("chat message", (message) => {
-        console.log(`Anonymous: ${message}`);
+        appendLi(`Anonymous User: ${message}`)
     });
 }
 
@@ -13,7 +14,29 @@ function sendMessage(message) {
      * Sends message to the socket backend server
      */
     socket.emit("chat message", message);
-    console.log(`YOU: ${message}`);
+}
+
+function appendLi(text) {
+    let newLi = document.createElement("li");
+    newLi.innerHTML = text;
+    messagesUl.appendChild(newLi);
+}
+
+function initInput() {
+    /**
+     * This function is ran to add eventlisteners to the
+     * input box
+     */
+    const runFunction = () => {
+        let message = messageBoxInput.value;
+        sendMessage(message);
+        appendLi(`You: ${message}`);
+        messageBoxInput.value = "";
+    };
+
+    messageBoxInput.addEventListener("keyup", event => {
+        if ("Enter" === event.key) runFunction();
+    });
 }
 
 main();
